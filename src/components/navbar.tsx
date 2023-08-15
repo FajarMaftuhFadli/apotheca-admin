5c3e218d1c150494afe0c7ead9b3d85ebc1b739f
Fajar Maftuh Fadli <fajarmaftuhfadli@gmail.com>
2023-08-15 11:02:54 +0700

    Refactor the NavBar component to include a StoreSwitcher component and add a check to redirect to the sign-in page if the user is not authenticated

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,12 +1,25 @@
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, auth } from "@clerk/nextjs";
 import NavMenu from "@/components/nav-menu";
+import StoreSwitcher from "@/components/store-switcher";
+import { redirect } from "next/navigation";
+import prismadb from "@/lib/prismadb";
 
-export default function NavBar() {
+export default async function NavBar() {
+  const { userId } = auth();
+
+  if (!userId) redirect("/sign-in");
+
+  const stores = await prismadb.store.findMany({
+    where: {
+      userId,
+    },
+  });
+
   return (
     <nav className="border-b">
       <div className="flex h-16 items-center justify-between px-4">
-        <div className="flex space-x-4">
-          <button className="bg-blue-400">Change store</button>
+        <div className="flex space-x-6">
+          <StoreSwitcher items={stores} />
           <NavMenu />
         </div>
         <div className="flex items-center justify-center space-x-4">
