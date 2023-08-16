68dfcb57e3571e9905dbc5a800bdeef862beef02
Fajar Maftuh Fadli <fajarmaftuhfadli@gmail.com>
2023-08-16 14:01:53 +0700

    remove unnessecary route grouping making it more simple

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -4,6 +4,6 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex h-full items-center justify-center">{children}</div>
+    <main className="flex h-full items-center justify-center">{children}</main>
   );
 }
