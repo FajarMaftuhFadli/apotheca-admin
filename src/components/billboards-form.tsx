6355c7c88cf471eb240cf8beeedb9fcafc9dba0f
Fajar Maftuh Fadli <fajarmaftuhfadli@gmail.com>
2023-08-22 15:16:52 +0700

    Remove unused import of ApiAlert in billboards-form.tsx

--- src/components/billboards-form.tsx
+++ src/components/billboards-form.tsx
@@ -23,7 +23,6 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import AlertModal from "@/components/modals/alert-modal";
-import ApiAlert from "@/components/api-alert";
 import { useOrigin } from "@/hooks/use-origin";
 import ImageUpload from "./image-upload";
 
