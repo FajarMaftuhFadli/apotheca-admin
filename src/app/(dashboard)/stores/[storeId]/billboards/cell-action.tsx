bb7ad9e94761da4cb93d0327a48379a25a4b2004
Fajar Maftuh Fadli <fajarmaftuhfadli@gmail.com>
2023-08-24 22:54:18 +0700

    Add loading and open state for delete confirmation modal and implement delete functionality for billboards

--- src/app/(dashboard)/stores/[storeId]/billboards/cell-action.tsx
+++ src/app/(dashboard)/stores/[storeId]/billboards/cell-action.tsx
@@ -3,6 +3,8 @@
 import { useParams, useRouter } from "next/navigation";
 import { Copy, Edit, MoreHorizontal, Trash } from "lucide-react";
 import toast from "react-hot-toast";
+import { useState } from "react";
+import axios from "axios";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -12,6 +14,7 @@ import {
   DropdownMenuLabel,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import AlertModal from "@/components/modals/alert-modal";
 
 import { BillboardsColumn } from "./columns";
 
@@ -20,15 +23,43 @@ interface CellActionProps {
 }
 
 export default function CellAction({ data }: CellActionProps) {
+  const [loading, setLoading] = useState(false);
+  const [open, setOpen] = useState(false);
+
   const router = useRouter();
   const param = useParams();
+
   const onCopy = () => {
     navigator.clipboard.writeText(data.id);
     toast.success("Copied to clipboard");
   };
 
+  const onDelete = async () => {
+    try {
+      setLoading(true);
+
+      await axios.delete(`/api/stores/${param.storeId}/billboards/${data.id}`);
+      router.refresh();
+
+      toast.success("Store deleted");
+    } catch (error) {
+      toast.error(
+        "Make sure you removed all categories using this billboard first",
+      );
+    } finally {
+      setLoading(false);
+      setOpen(false);
+    }
+  };
+
   return (
     <>
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        loading={loading}
+        onConfirm={onDelete}
+      />
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button variant="ghost" className="h-8 w-8 p-0">
@@ -48,7 +79,7 @@ export default function CellAction({ data }: CellActionProps) {
           >
             <Edit className="mr-2 h-4 w-4" /> Edit
           </DropdownMenuItem>
-          <DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setOpen(true)}>
             <Trash className="mr-2 h-4 w-4" /> Delete
           </DropdownMenuItem>
         </DropdownMenuContent>
